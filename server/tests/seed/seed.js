ac913e8f245c5f78247d6116dcfc51aab90dc758
jis3613 <jis3613@gmail.com>
2017-07-08 23:12:57 +0200

    Final

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -1,4 +1,4 @@
-const{ObjectID} = require('mongodb');
+const {ObjectID} = require('mongodb');
 const jwt = require('jsonwebtoken');
 
 const {Todo} = require('./../../models/todo');
@@ -6,29 +6,34 @@ const {User} = require('./../../models/user');
 
 const userOneId = new ObjectID();
 const userTwoId = new ObjectID();
-
 const users = [{
   _id: userOneId,
-  email: 'tommy@example.com',
+  email: 'andrew@example.com',
   password: 'userOnePass',
   tokens: [{
     access: 'auth',
-    token: jwt.sign({_id: userOneId, access: 'auth'}, 'abc123').toString()
+    token: jwt.sign({_id: userOneId, access: 'auth'}, process.env.JWT_SECRET).toString()
   }]
-},{
+}, {
   _id: userTwoId,
   email: 'jen@example.com',
-  password: 'userTwoPass'
+  password: 'userTwoPass',
+  tokens: [{
+    access: 'auth',
+    token: jwt.sign({_id: userTwoId, access: 'auth'}, process.env.JWT_SECRET).toString()
+  }]
 }];
 
 const todos = [{
   _id: new ObjectID(),
-  text: 'First test todo'
+  text: 'First test todo',
+  _creator: userOneId
 }, {
   _id: new ObjectID(),
   text: 'Second test todo',
   completed: true,
-  completedAt: 333
+  completedAt: 333,
+  _creator: userTwoId
 }];
 
 const populateTodos = (done) => {
@@ -42,8 +47,8 @@ const populateUsers = (done) => {
     var userOne = new User(users[0]).save();
     var userTwo = new User(users[1]).save();
 
-    return Promise.all([userOne,userTwo])
-  }).then(()=>done());
+    return Promise.all([userOne, userTwo])
+  }).then(() => done());
 };
 
 module.exports = {todos, populateTodos, users, populateUsers};
