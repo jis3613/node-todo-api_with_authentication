e250fb409613e907e6d5e3ba25a6c1071810b82c
jis3613 <jis3613@gmail.com>
2017-07-09 01:38:55 +0200

    comment added

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ var {User} = require('./models/user');
 var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
-const port = process.env.PORT;
+const port = process.env.PORT;//process.env is an object all of our enviroment variabels as key value pair.
 
 app.use(bodyParser.json());
 
